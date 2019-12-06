e8242948900219dba8e992250316f4864a3b8cc6
Brandon Chasser <brandonchasser22@gmail.com>
2019-12-06 12:20:08 -0500

    Comments

--- js/planet.js
+++ js/planet.js
@@ -1,3 +1,4 @@
+// three.js vars
 var scene,
     camera,
     renderer,
@@ -6,7 +7,7 @@ var scene,
 
 var radius = 35;
 
-
+// setup of planet
 function planet(r) {
     var groundMaterial = new THREE.MeshLambertMaterial({
         color: 0x634b35
@@ -20,16 +21,17 @@ function planet(r) {
 
 function init() {
 
-    
+    // setup scene
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 80;
 
+    // render planet
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    
+    // lights for the planet
     var light1 = new THREE.DirectionalLight(0xDDEED3, 1);
     var light2 = new THREE.AmbientLight(0x7D7D7D);
     light1.position.set(0, 0, 1);
@@ -38,12 +40,12 @@ function init() {
     scene.add(light2);
     scene.add(planet(radius));
 
-    
+    // orbit controls
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.autoRotate = true;
     controls.update();
 }
-
+// display planet
 function render() {
     requestAnimationFrame(render);
     controls.update();
