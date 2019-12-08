51678ffc4caae4a0709adb904124b910cd5f92fe
Brandon Chasser <brandonchasser22@gmail.com>
2019-12-08 14:52:05 -0500

    Update colors/shading

--- js/planet.js
+++ js/planet.js
@@ -8,25 +8,25 @@ var scene,
 
 // geometry of buildings
 var buildingMaterial = new THREE.MeshLambertMaterial({
-    color: 0xe3dca1
+    color: 0xe5ff00
 });
-var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+var cubeGeometry = new THREE.BoxGeometry(0, 0, 0);
 
-var radius = 40;
+var radius = 50;
 // setup of planet
 function planet(r) {
     var groundMaterial = new THREE.MeshLambertMaterial({
-        color: 0x7d715b
+        color: 0x545050
     });
     var planetGeometry = new THREE.SphereGeometry(radius, 100, 100);
     var planet = new THREE.Mesh(planetGeometry, groundMaterial);
-    planet.position.set(0, 0, 0);
+    planet.position.set(1, 0, 0);
     scene.add(planet);
 }
 // setup of buildings
 function building(x, y, z) {
     var structure = new THREE.Mesh(cubeGeometry, buildingMaterial);
-    structure.position.set(0, radius + 0.75, 0);
+    structure.position.set(0, radius + 0.1, 0);
     structure.scale.set(0.3, 1.5, 0.3);
 
 
@@ -45,13 +45,14 @@ function building(x, y, z) {
 function init() {
     // setup scene
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 80;
 
     // lights for the planet
-    var light1 = new THREE.DirectionalLight(0x403f3e);
-    var light2 = new THREE.AmbientLight(0xadbcff);
+    var light1 = new THREE.DirectionalLight(0x6e6e6e);
+    var light2 = new THREE.AmbientLight(0x6e6e6e);
     light1.position.set(0, 0, 1);
+    light2.position.set(1, 0, 0);
 
     scene.add(light1);
     scene.add(light2);
